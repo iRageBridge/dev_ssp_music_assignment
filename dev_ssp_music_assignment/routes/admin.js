bcbbd83f338673f6d6f221e896f2670b9ac95eb8
Simon Standbridge <simonstandbridge@gmail.com>
2017-03-21 13:54:35 +0000

    added track functionality

--- dev_ssp_music_assignment/routes/admin.js
+++ dev_ssp_music_assignment/routes/admin.js
@@ -16,29 +16,28 @@ router.get('/', function(req, res, next) {
   dbConnection.on('error', function(err) {
     if (err.code == 'PROTOCOL_SEQUENCE_TIMEOUT') {
       console.log('Got a DB PROTOCOL_SEQUENCE_TIMEOUT Error ... ignoring ');
-    } else {
+    } 
+    else {
       console.log('Got a DB Error: ', err);
     }
   });
 
   dbConnection.query('SELECT * FROM playlists', function(err, results, fields){
-  if (err) {
-    throw err;
-  }
+    if (err) {
+      throw err;
+    }
 
-  var allPlaylists = new Array();
-  for(var i=0; i<results.length; i++){
-    var playlist={};
-    playlist.id = results[i].id;
-    playlist.name = results[i].name;
-    playlist.userId = results[i].user_userId;
+    var allPlaylists = new Array();
+    for(var i=0; i<results.length; i++){
+      var playlist={};
+      playlist.id = results[i].playlistId;
+      playlist.name = results[i].playlistName;
+      playlist.userId = results[i].user_userId;
 
-    allPlaylists.push(playlist);
+      allPlaylists.push(playlist);
     }
     dbConnection.end();
-    res.render('playlists',{
-      playlists: allPlaylists
-    });
+    res.render('playlists',{playlists: allPlaylists});
   });
 });
 
@@ -69,6 +68,72 @@ router.post('/newPlaylist', function(req,res,next){
 
     dbConnection.end();
     res.redirect('/admin');
+  });
+});
+
+router.get('/playlist',function(req,res,next){
+  var dbConnection = mysql.createConnection(databaseInfo);
+  dbConnection.connect();
+  dbConnection.on('error', function(err) {
+    if (err.code == 'PROTOCOL_SEQUENCE_TIMEOUT') {
+      console.log('Got a DB PROTOCOL_SEQUENCE_TIMEOUT Error ... ignoring ');
+    } 
+    else {
+      console.log('Got a DB Error: ', err);
+    }
+  });
+
+  dbConnection.query('SELECT * FROM tracks', function(err, results, fields){
+    if (err) {
+      throw err;
+    }
+
+    var allTracks = new Array();
+    for(var i=0; i<results.length; i++){
+      var track={};
+      track.id = results[i].trackId;
+      track.name = results[i].trackName;
+      track.url = results[i].trackURL;
+
+      allTracks.push(track);
+    }
+    dbConnection.end();
+    res.render('playlist',{tracks: allTracks});
+  });
+});
+
+router.post('/playlist', function(req,res,next){
+    
+});
+
+router.get('/newSound', function(req,res,next){
+  res.render('newSound');
+});
+
+router.post('/newSound',function(req,res,next){
+  var dbConnection = mysql.createConnection(databaseInfo);
+  dbConnection.connect();
+  dbConnection.on('error', function(err) {
+    if (err.code == 'PROTOCOL_SEQUENCE_TIMEOUT') {
+      console.log('Got a DB PROTOCOL_SEQUENCE_TIMEOUT Error ... ignoring ');
+    } else {
+      console.log('Got a DB Error: ', err);
+    }
+  });
+
+  var track={};
+  track.name=req.body.trackName;
+  track.url = req.body.trackURL;
+
+  dbConnection.query('INSERT INTO tracks (trackName,trackURL) VALUES(?,?)',[track.name,track.url],function(err,results,fields){
+    if(err){
+      throw err;
+    }
+
+    track.id = results.inserdId;
+
+    dbConnection.end();
+    res.redirect('/playlist');
   });
 });
 
