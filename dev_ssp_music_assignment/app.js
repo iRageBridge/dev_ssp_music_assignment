a8a96c8473dd8820078a694f143098151c46b9b4
Simon Standbridge <simonstandbridge@gmail.com>
2017-03-21 10:16:53 +0000

    init

---
+++ dev_ssp_music_assignment/app.js
@@ -0,0 +1,63 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var admin = require('./routes/admin');
+
+var app = express();
+var session = require('express-session');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+var expressSession={
+  secret:'mySecret',
+  resave:'false',
+  saveUninitialized: false
+}
+app.use(session(expressSession));
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var loggedIn=function(req,res,next){
+  if(!req.session.username){
+    res.redirect('/login');
+  }
+  else{
+    next();
+  }
+};
+
+app.use('/', routes);
+app.use('/admin',admin);
+app.use('/admin',loggedIn);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
